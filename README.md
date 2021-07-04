d8c266756ccd289e3e96bb2d3962a8d3b8a4c267
Danish <danishkarim96@gmail.com>
2021-07-04 16:27:03 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+React-Portfolio is being sponsored by the following tool; please help to support us by taking a look and signing up to a free trial
+
+<a href="https://tracking.gitads.io/?repo=React-Portfolio"><img src="https://images.gitads.io/React-Portfolio" alt=“GitAds”/></a>
+
+
+
+
+A simple SPA Portfolio template for developer/designers built with React. Use it to showcase your work, testimonials and other information to clients.
+
+## Preview
+![Preview](https://ibb.co/fSGwDy5)
+
+## How to use
+1. Clone/Download the repo.
+2. Run  ``` npm install ```.
+3. Change the values in ```src/resumeData.js``` to suit your use-case.
+4. Run ```npm start``` to spin the up the local dev server port 3000.(http://localhost:3000).
+5. Make required changes in ```src/resumeData.js``` to suit your needs.
+
---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/Danish-karim.png",
+    "name": "Danish karim",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/danish-ciit-aab681128",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Danish-karim",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://join.skype.com/invite/peara6gidmhh",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Hi I am Danish karim with a Bachelors degree in Computer science.I have expertise in front end development that includes javascript,React js and redux. I am passionate about web development and will provide quality work.",
+    "address":"Pakistan",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"COMSATS University Islamabad-Abbottabad Campus",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project1.png"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project3.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project4.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
